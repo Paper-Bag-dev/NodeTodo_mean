ab7cdc3e296b845f31d8a8e01034d6baa3877cee
Vikalp Sharma <vikalpsh1234@gmail.com>
2023-10-13 21:36:14 +0530

    reverted

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*",
+    origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: false,
+    credentials: true,
   })
 );
 
